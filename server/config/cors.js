c6385e11c79a23f30336194d5278ce0ef800ed85
phatnguyen03022001 <phatnguyen03022001@gmail.com>
2025-03-06 13:05:10 +0700

    update to deploy to production environment

--- server/config/cors.js
+++ server/config/cors.js
@@ -1,5 +1,8 @@
 module.exports = {
-  origin: "http://localhost:3000" || process.env.CLIENT_URL,
+  origin: [
+    process.env.CLIENT_URL, // URL của frontend trên môi trường production (lấy từ .env)
+    "http://localhost:3000", // URL của frontend trên môi trường development
+  ],
   // origin: '*',
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
